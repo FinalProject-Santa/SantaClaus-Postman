6dc9b4a4d661877db4bd7d023a0ac237c47f7962
jiyeeon <yeoniiiiiiiiiii@gmail.com>
2023-01-05 22:31:50 +0900

    map으로 insert

--- src/main/java/com/boot/jdbc/model/dao/DiaryMapper.java
+++ src/main/java/com/boot/jdbc/model/dao/DiaryMapper.java
@@ -1,5 +1,7 @@
 package com.boot.jdbc.model.dao;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -10,8 +12,14 @@ public interface DiaryMapper {
 	
 	@Insert("INSERT INTO DIARY VALUES(NULL,#{user_id},#{kids_no},#{diary_content},NOW(),#{phone})")
 	int insert(DiaryDto dto);
+	
+	@Insert("INSERT INTO DIARY VALUES(NULL,#{userID},#{kidsNO},#{filePath},NOW(),#{phoneNum})")
+	int saveImg(Map<Object, Object> param);
 }
 
+//"userID":userId,
+//"kidsNO":kidsNo,
+//"phoneNum":phone
 //private int diary_no;
 //private String user_id;
 //private int kids_no;
