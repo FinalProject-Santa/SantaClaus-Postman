e74d388e3acce26dd7684b26595590bcb9a8308a
younggyung <melondumpling01@gmail.com>
2023-01-11 16:15:05 +0900

    1/11 우영경

--- src/main/java/com/boot/jdbc/controller/CustomerController.java
+++ src/main/java/com/boot/jdbc/controller/CustomerController.java
@@ -2,45 +2,41 @@ package com.boot.jdbc.controller;
 
 
 import java.io.File;
-import java.util.UUID;
-
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.boot.jdbc.model.biz.ArticleBiz;
 import com.boot.jdbc.model.biz.QnaBiz;
 import com.boot.jdbc.model.dto.QnaDto;
+import com.boot.jdbc.model.dto.QnaFileDto;
 
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	//String uploadPath = "C:\\Users\\Home\\git\\SantaClaus-Postman\\src\\main\\resources\\static\\files";
+
 	
-	private static Logger logger = LoggerFactory.getLogger(CustomerController.class);
+	//private static Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
 	@Autowired
 	private ArticleBiz articlebiz;
 	@Autowired
 	private QnaBiz qnabiz;
 	
-	@Value("${upload.path}")
-	private String uploadPath;
+//	@Value("${upload.path}")
+//	private String uploadPath;
 	
 	@GetMapping("/faq")
 	public String faq() {
@@ -82,101 +78,53 @@ public class CustomerController {
 	}
 	
 	@PostMapping("/qnainsert")
-	public String insert(QnaDto dto, Model model) throws Exception{
-		//보내진 파일 file 변수에 받아주기
-		MultipartFile file = dto.getQna_file();
-		model.addAttribute("dto", dto);
+	public String Insert(QnaDto dto, @RequestPart MultipartFile files) throws Exception{
 		
-		logger.info("OriginalName:" + file.getOriginalFilename());
-		logger.info("size:" + file.getSize());
-		logger.info("contentType" + file.getContentType());
+		QnaFileDto file = new QnaFileDto();
 		
-		//uploadFile 메소드 이용해서 중복되지않는 파일이름과, 바인딩, 지정된 폴더로 카피까지 처리
-		String createdFileName = uploadFile(file.getOriginalFilename(),file.getBytes());
-		dto.setQna_filepath(createdFileName);
+		String uploadPath = "C:/Users/Home/git/SantaClaus-Postman/src/main/resources/static/files/";
+		String sourceFileName = files.getOriginalFilename(); 
+        String sourceFileNameExtension = FilenameUtils.getExtension(sourceFileName).toLowerCase(); 
+        File destinationFile; 
+        String destinationFileName;
+        
+        do { 
+            destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + sourceFileNameExtension; 
+            destinationFile = new File(uploadPath + destinationFileName); 
+        } while (destinationFile.exists()); 
+        
+        destinationFile.getParentFile().mkdirs(); 
+        files.transferTo(destinationFile); 
+        
+		qnabiz.insert(dto);
+		System.out.println(dto.getQna_no());
 		
-			if(qnabiz.insert(dto)>0) {
-				return "redirect:/customer/qnalist";
-			}else {
-				return "redirect:/customer/qnainsert";
-			}
-		} 
-		
+		 file.setQna_no(dto.getQna_no());
+         file.setFile_name(destinationFileName);
+         file.setFile_oname(sourceFileName);
+         file.setFile_path(uploadPath);
+         
+         qnabiz.insertFile(file); 
+         return "redirect:/customer/qnalist";
+    }
+	
 	
 
 	@GetMapping("/qnadetail")
-	public String qnadetail(Model model, Integer qna_no) {
+	public String qnadetail(Model model,Integer qna_no) {
 		model.addAttribute("dto",qnabiz.selectOne(qna_no));
+		model.addAttribute("files",qnabiz.selectFile(qna_no)); // 파일경로
 		return "customer/qnadetail";
 				
 	}
 	
-	private String uploadFile(String originalName, byte[] fileData) throws Exception {
-		//중복되지 않는 난수 구하기
-		UUID uid = UUID.randomUUID();
-		//업로드 된 파일명에 uid붙여서 중복되지 않게 새로운 이름 만들어주기
-		String createdFileName = uid.toString() + "_" + originalName;
-		//File(빈파일)타입 target에 에 업로드될 디렉토리와 저장된 파일명 저장해서
-		File target = new File(uploadPath,createdFileName);
-		//fileDate로 바인딩 한 후, 지정된 폴더로 카피하기 ->다운로드 요청이 오면 이 정보로 저장된 파일을 읽는다.
-		FileCopyUtils.copy(fileData, target);
-		return createdFileName;
-		}
-	
-//	@ResponseBody
-//	@GetMapping("/qnadetail")
-//	public ResponseEntity<byte[]> displayFile(Integer qna_no) throws Exception {
-//	ResponseEntity<byte[]> entity = null;
-//
-//	String fileName = qnabiz.getPicture(qna_no);
-//
-//	logger.info("FILE NAME: " + fileName);
-//
-//	try {
-//
-//	String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
-//
-//	MediaType mediaType = getMediaType(formatName);
-//
-//	HttpHeaders headers = new HttpHeaders();
-//
-//	File file = new File(uploadPath + File.separator + fileName);
-//
-//	if (mediaType != null) {
-//	headers.setContentType(mediaType);
-//	}
-//
-//	entity = new ResponseEntity<byte[]>(FileCopyUtils.copyToByteArray(file), headers, HttpStatus.CREATED);
-//	} catch (Exception e) {
-//	e.printStackTrace();
-//	entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
-//	}
-//
-//	return entity;
-//	}
-//
-//	private MediaType getMediaType(String formatName){
-//	if(formatName != null) {
-//	if(formatName.equals("JPG")) {
-//	return MediaType.IMAGE_JPEG;
-//	}
-//
-//	if(formatName.equals("GIF")) {
-//	return MediaType.IMAGE_GIF;
-//	}
-//
-//	if(formatName.equals("PNG")) {
-//	return MediaType.IMAGE_PNG;
-//	}
-//	}
-//
-//	return null;
-//	}
-//	
-	
-	
 	
 }
+	
+	
+	
+	
+
 
 
 	
--- src/main/java/com/boot/jdbc/model/dao/QnaMapper.java
+++ src/main/java/com/boot/jdbc/model/dao/QnaMapper.java
@@ -1,16 +1,20 @@
 package com.boot.jdbc.model.dao;
 
 import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import com.boot.jdbc.model.dto.QnaDto;
+import com.boot.jdbc.model.dto.QnaFileDto;
 
 @Mapper
 public interface QnaMapper {
 	
-	@Insert("INSERT INTO QNA(USER_ID, QNA_TYPE, QNA_TITLE, QNA_DATE, QNA_CONTENT, QNA_FILEPATH) VALUES (#{user_id}, #{qna_type}, #{qna_title}, NOW(), #{qna_content}, #{qna_filepath})")
+	@Insert("INSERT INTO QNA VALUES (#{qna_no},#{user_id}, #{qna_type}, #{qna_title}, NOW(), #{qna_content} )")
+	@Options(useGeneratedKeys = true, keyProperty = "qna_no")
 	int insert(QnaDto dto);
 	
 	@Select("SELECT * FROM QNA ORDER BY QNA_NO DESC")
@@ -20,7 +24,16 @@ public interface QnaMapper {
 	QnaDto selectOne(Integer qna_no);
 
 	
-	@Select("SELECT QNA_FILEPATH FROM QNA WHERE QNA_NO=#{qna_no}")
-	String getPicture(Integer qna_no);
+	@Select("SELECT * FROM qnaFile WHERE QNA_NO=#{qna_no}")
+	QnaDto getPicture(Integer qna_no);
 
+	@Select("SELECT * FROM qnaFile")
+	QnaDto selectAll();
+
+	@Insert("INSERT INTO qnaFile(qna_no, file_id, file_oname,file_name,file_path) VALUES (#{qna_no}, #{file_id}, #{file_oname},#{file_name},#{file_path})")
+	@Options(useGeneratedKeys = true, keyProperty = "file_id")
+	int insertFile(QnaFileDto file);
+
+	@Select("SELECT * FROM qnaFile WHERE QNA_NO=#{qna_no}")
+	QnaFileDto selectFile(Integer qna_no);
 }
