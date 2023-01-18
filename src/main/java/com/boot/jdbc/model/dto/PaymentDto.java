814f525786cf0a5fe15f219a4f7bb108c744c0d9
pota3868 <pota_@naver.com>
2023-01-18 21:35:34 +0900

    제명리 주문조회 부분 : order -> orderInfo 변경

--- src/main/java/com/boot/jdbc/model/dto/PaymentDto.java
+++ src/main/java/com/boot/jdbc/model/dto/PaymentDto.java
@@ -3,97 +3,154 @@ package com.boot.jdbc.model.dto;
 import java.util.Date;
 
 public class PaymentDto {
-	private int orderno;
+	private int order_no;
 	private String user_id;
-	private int template_no;
+	private int letter_no;
 	private int option_no;
+	private Date order_date;
 	private String receiver;
 	private String email;
-	private String addr;
+	private String post_code;
+	private String default_addr;
+	private String detail_addr;
 	private String phone;
-	private Date deliverydate;
+	private String delivery_message;
+	private Date delivery_date;
 	private int point;
-
 	
 	public PaymentDto() {
 		super();
 	}
-	public PaymentDto(int orderno, String user_id, int template_no, int option_no, String receiver, String email,
-			String addr, String phone, Date deliverydate, int point) {
+
+	public PaymentDto(int order_no, String user_id, int letter_no, int option_no, Date order_date, String receiver,
+			String email, String post_code, String default_addr, String detail_addr, String phone,
+			String delivery_message, Date delivery_date, int point) {
 		super();
-		this.orderno = orderno;
+		this.order_no = order_no;
 		this.user_id = user_id;
-		this.template_no = template_no;
+		this.letter_no = letter_no;
 		this.option_no = option_no;
+		this.order_date = order_date;
 		this.receiver = receiver;
 		this.email = email;
-		this.addr = addr;
+		this.post_code = post_code;
+		this.default_addr = default_addr;
+		this.detail_addr = detail_addr;
 		this.phone = phone;
-		this.deliverydate = deliverydate;
+		this.delivery_message = delivery_message;
+		this.delivery_date = delivery_date;
 		this.point = point;
 	}
-	public int getOrderno() {
-		return orderno;
+
+	public int getOrder_no() {
+		return order_no;
 	}
-	public void setOrderno(int orderno) {
-		this.orderno = orderno;
+
+	public void setOrder_no(int order_no) {
+		this.order_no = order_no;
 	}
+
 	public String getUser_id() {
 		return user_id;
 	}
+
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
 	}
-	public int getTemplate_no() {
-		return template_no;
+
+	public int getLetter_no() {
+		return letter_no;
 	}
-	public void setTemplate_no(int template_no) {
-		this.template_no = template_no;
+
+	public void setLetter_no(int letter_no) {
+		this.letter_no = letter_no;
 	}
+
 	public int getOption_no() {
 		return option_no;
 	}
+
 	public void setOption_no(int option_no) {
 		this.option_no = option_no;
 	}
+
+	public Date getOrder_date() {
+		return order_date;
+	}
+
+	public void setOrder_date(Date order_date) {
+		this.order_date = order_date;
+	}
+
 	public String getReceiver() {
 		return receiver;
 	}
+
 	public void setReceiver(String receiver) {
 		this.receiver = receiver;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getAddr() {
-		return addr;
+
+	public String getPost_code() {
+		return post_code;
 	}
-	public void setAddr(String addr) {
-		this.addr = addr;
+
+	public void setPost_code(String post_code) {
+		this.post_code = post_code;
 	}
+
+	public String getDefault_addr() {
+		return default_addr;
+	}
+
+	public void setDefault_addr(String default_addr) {
+		this.default_addr = default_addr;
+	}
+
+	public String getDetail_addr() {
+		return detail_addr;
+	}
+
+	public void setDetail_addr(String detail_addr) {
+		this.detail_addr = detail_addr;
+	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	public Date getDeliverydate() {
-		return deliverydate;
+
+	public String getDelivery_message() {
+		return delivery_message;
 	}
-	public void setDeliverydate(Date deliverydate) {
-		this.deliverydate = deliverydate;
+
+	public void setDelivery_message(String delivery_message) {
+		this.delivery_message = delivery_message;
 	}
+
+	public Date getDelivery_date() {
+		return delivery_date;
+	}
+
+	public void setDelivery_date(Date delivery_date) {
+		this.delivery_date = delivery_date;
+	}
+
 	public int getPoint() {
 		return point;
 	}
+
 	public void setPoint(int point) {
 		this.point = point;
 	}
-	
-	
-	 
-	
 }
