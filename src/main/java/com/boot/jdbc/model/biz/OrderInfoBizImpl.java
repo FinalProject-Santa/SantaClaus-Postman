253fc9161ad46e25303f9511e4e132aae4c131aa
tmdwpckd <tmdwpckd@nate.com>
2023-02-01 05:54:45 +0900

    2.1 주문조회 -> 리뷰작성 연결

--- src/main/java/com/boot/jdbc/model/biz/OrderInfoBizImpl.java
+++ src/main/java/com/boot/jdbc/model/biz/OrderInfoBizImpl.java
@@ -7,10 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.boot.jdbc.model.dao.OrderInfoMapper;
-import com.boot.jdbc.model.dto.LetterDto;
 import com.boot.jdbc.model.dto.MemberDto;
 import com.boot.jdbc.model.dto.OrderDto;
-import com.boot.jdbc.model.dto.OrderInfoDto;
+import com.boot.jdbc.model.dto.ReviewDto;
 
 @Service
 public class OrderInfoBizImpl implements OrderInfoBiz{
@@ -19,7 +18,7 @@ public class OrderInfoBizImpl implements OrderInfoBiz{
 	private OrderInfoMapper orderMapper;
 	
 	@Override
-	public List<OrderInfoDto> orderList(@Param("memberDto")MemberDto memberDto, @Param("startdate")String startdate, @Param("enddate")String enddate) {
+	public List<OrderDto> orderList(@Param("memberDto")MemberDto memberDto, @Param("startdate")String startdate, @Param("enddate")String enddate) {
 		return orderMapper.orderList(memberDto, startdate, enddate);
 	}
 
@@ -28,31 +27,17 @@ public class OrderInfoBizImpl implements OrderInfoBiz{
 		return orderMapper.orderDtoList(user_id);
 	}
 
-//	@Override
-//	public List<OrderDto> selectOrder() {
-//		
-//		return orderMapper.selectOrder();
-//	}
+	@Override
+	public OrderDto orderdto(String user_id) {
+		return orderMapper.orderdto(user_id);
+	}
 
-//	@Override
-//	public LetterTemplateDto templateDetail(int template_no) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
+	@Override
+	public ReviewDto selectReview(String order_no) {
+		return orderMapper.selectReview(order_no);
+	}
 
-//	@Override
-//	public OptionDto optionDetail(int option_no) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
 
-	
-	//결제 창에서 결제하기 눌렀을 때 삽입 실행
-//	@Override
-//	public int orderInsert(OrderDto orderdto) {
-//		// TODO Auto-generated method stub
-//		return 0;
-//	}
 
 
 }
--- src/main/java/com/boot/jdbc/model/dao/OrderInfoMapper.java
+++ src/main/java/com/boot/jdbc/model/dao/OrderInfoMapper.java
@@ -2,37 +2,39 @@ package com.boot.jdbc.model.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.boot.jdbc.model.dto.MemberDto;
 import com.boot.jdbc.model.dto.OrderDto;
-import com.boot.jdbc.model.dto.OrderInfoDto;
+import com.boot.jdbc.model.dto.ReviewDto;
 
 
 @Mapper
 public interface OrderInfoMapper {
-	@Select(" SELECT * FROM `ORDER_INFO` WHERE USER_ID= #{memberDto.user_id} AND ORDER_DATE BETWEEN #{startdate} AND #{enddate} ORDER BY ORDER_DATE DESC ")
-	List<OrderInfoDto> orderList(@Param("memberDto")MemberDto memberDto, @Param("startdate")String startdate, @Param("enddate")String enddate);
+	@Select(" SELECT * FROM `ORDER` WHERE USER_ID= #{memberDto.user_id} AND ORDER_DATE BETWEEN #{startdate} AND #{enddate} ORDER BY ORDER_DATE DESC ")
+	List<OrderDto> orderList(@Param("memberDto")MemberDto memberDto, @Param("startdate")String startdate, @Param("enddate")String enddate);
 
 	
-	@Select(" SELECT * FROM `ORDER` WHERE USER_ID=#{user_id}")
+	@Select(" SELECT * FROM `ORDER` WHERE USER_ID=#{user_id} ORDER BY ORDER_DATE DESC ")
 	List<OrderDto> orderDtoList(String user_id);
+	
+	@Select(" SELECT * FROM `ORDER` WHERE USER_ID=#{user_id} ORDER BY ORDER_DATE DESC ")
+	OrderDto orderdto(String user_id);
+	
 //	@Select(" SELECT * FROM ORDER WHERE ORDER_DATE BETWEEN #{ORDER_DATE ")
 //	List<OrderDto> selectOrder();
-
 	
-//	@Select(" SELECT * FROM TEMPLATE WHERE TEMPLATE_NO=#{template_no} ")
-//	LetterTemplateDto templateDetail(int template_no);
+	@Select(" SELECT REVIEW_NO FROM REVIEW WHERE ORDER_NO=#{order_no} ")
+	ReviewDto selectReview(String order_no);
+
 
 //	@Select(" SELECT * FROM OPTION WHERE OPTION_NO=#{option_no} ")
 //	OptionDto optionDetail(int template_no);
 	
 	
-	
-//	@Insert(" INSERT INTO ORDER VALUES(#{order_no}, #{user_id}, NOW(), #{review_status}, #{delivery_status}, #{product_quantity}, #{product_cost}) ")
-//	int orderInsert(OrderDto orderdto);
 	
 	
 	
